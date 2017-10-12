1ca21c3b98bd5db6f134b9ce7393453c8ae9441c
CIngram82 <cingram82@gmail.com>
2017-10-11 23:17:08 -0400

    menu state added

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,38 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.FlappyDemo;
+
+/**
+ * Created by cingr on 10/11/2017.
+ */
+
+public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0,0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+        sb.draw(playBtn,((FlappyDemo.WIDTH /2) - (playBtn.getWidth()/2)), ((FlappyDemo.HEIGHT /2) - (playBtn.getHeight()/2)));
+        sb.end();
+    }
+}
