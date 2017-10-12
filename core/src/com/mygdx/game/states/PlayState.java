109e3b5f0650242f0550ac4862f366133852959d
CIngram82 <cingram82@gmail.com>
2017-10-12 18:20:40 -0400

    BG and tubes at random height added

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,8 +1,11 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.FlappyDemo;
 import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
 
 /**
  * Created by cingr on 10/12/2017.
@@ -10,17 +13,25 @@ import com.mygdx.game.sprites.Bird;
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
+
 
     @Override
     protected void handleInput() {
-
+    if(Gdx.input.justTouched()){
+        bird.jump();
+    }
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth/2),0);
         sb.draw(bird.getTexture(), bird.getPosition().x,bird.getPosition().y);
+        sb.draw(tube.getTopTube(),tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
         sb.end();
     }
 
@@ -40,5 +51,7 @@ public class PlayState extends State {
         super(gsm);
         bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyDemo.WIDTH/2, FlappyDemo.HEIGHT/2);
+        bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 }
